79ee00fffdfc196382fc3c511093a5d03525aa3a
Andrew Patipaksiri <apax714@gmail.com>
2016-06-10 10:17:48 -0700

    [setup] connect server with mongodb

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 	//morgan and bodyparser are middleware
