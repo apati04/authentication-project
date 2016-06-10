8816944a3a3d7da19d79bfa08f88745c57d16f9d
Andrew Patipaksiri <apax714@gmail.com>
2016-06-10 11:31:48 -0700

    [feature] unique email signup

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,10 +1,31 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
+	const email = req.body.email;
+	const password = req.body.password;
+
 	// see if a user with given email exists
-	
-	// if a user with email does exist, return an error
-	
+	User.findOne({ email: email }, function(err, existingUser){
+		if (err) { return next(err); }
+
+		// if a user with email does exist, return an error
+		if (existingUser) {
+			// status code 422 -> unprocessable entity
+			return res.status(422).send( { error: 'Email is in use' });
+		}
 	// if a user with email does NOT exist, create and save user record
+		const user = new User({
+			email: email,
+			password: password
+		});
+
+		user.save(function(err) {
+			if (err) { return next(err); }
+			// respond to req indicating the user was create
+			res.json(user);
+		});
+	})
+		
 	
-	// respond to req indicating the user was create
-	res.send({ success: 'true' })
+
 }
