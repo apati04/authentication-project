2449d6b14eb464833ee0dbc7e21eec3ae0bf3c94
Andrew Patipaksiri <apax714@gmail.com>
2016-06-10 10:35:57 -0700

    [setup] create controller folder and setup POST

---
+++ server/controllers/authentication.js
@@ -0,0 +1,10 @@
+exports.signup = function(req, res, next) {
+	// see if a user with given email exists
+	
+	// if a user with email does exist, return an error
+	
+	// if a user with email does NOT exist, create and save user record
+	
+	// respond to req indicating the user was create
+	res.send({ success: 'true' })
+}
