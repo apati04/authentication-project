d8d898e33cee6c0cc13dcd1c199a82cc42570118
Andrew P <apak@apak.local>
2016-06-10 09:39:30 -0700

    [setup] server files and dependenices

---
+++ server/index.js
@@ -0,0 +1,20 @@
+// main starting point
+// start server with node index.js
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+
+// App setup
+	//morgan and bodyparser are middleware
+app.use(morgan('combined')); // logger
+app.use(bodyParser.json({ type: '*/*' })); // all requests parsed into json
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on: ', port);
---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //fields about models
+
+// Define our model. unique: true sets the email created to be unique. mongoose does not account for case so lowercase true
+const userSchema = new Schema({
+	email: { type: String, unique: true, lowercase: true },
+	password: String
+});
+
+// Create the model class
+
+
+// Export the model
